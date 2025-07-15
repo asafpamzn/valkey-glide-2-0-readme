562425c48af92e1eff8f6df05af07ab95bdbd723
asafpamzn <97948347+asafpamzn@users.noreply.github.com>
2025-07-15 10:04:57 +0300

    updates

--- php/valkey_glide_cluster.c
+++ php/valkey_glide_cluster.c
@@ -1,19 +1,4 @@
-/*
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2009 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: Michael Grunder <michael.grunder@gmail.com>                  |
-  | Maintainer: Nicolas Favre-Felix <n.favre-felix@owlient.eu>           |
-  +----------------------------------------------------------------------+
-*/
+/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
